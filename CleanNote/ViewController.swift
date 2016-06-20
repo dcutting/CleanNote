f15c155829e02e10a1a1331c545928d0a1907f8b
Dan Cutting <dan@cutting.io>
2016-06-20 17:21:23 +0100

    Tidy,

--- CleanNote/ViewController.swift
+++ CleanNote/ViewController.swift
@@ -1,25 +1,4 @@
-//
-//  ViewController.swift
-//  CleanNote
-//
-//  Created by Dan Cutting on 20/06/2016.
-//  Copyright © 2016 cutting.io. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
