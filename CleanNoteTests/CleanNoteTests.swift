0e08d5a642698d195211a4214a52106cc5ca9779
Dan Cutting <dan@cutting.io>
2016-06-20 17:20:20 +0100

    Initial Commit

---
+++ CleanNoteTests/CleanNoteTests.swift
@@ -0,0 +1,36 @@
+//
+//  CleanNoteTests.swift
+//  CleanNoteTests
+//
+//  Created by Dan Cutting on 20/06/2016.
+//  Copyright © 2016 cutting.io. All rights reserved.
+//
+
+import XCTest
+@testable import CleanNote
+
+class CleanNoteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
