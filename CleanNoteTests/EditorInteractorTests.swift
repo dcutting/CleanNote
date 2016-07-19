888b214071e29724c75234d95c07b5ad84cb1134
Dan Cutting <dan@cutting.io>
2016-07-19 16:25:46 +0100

    EditorInteractorTests: test_fetchText_existingNote_verbatimText.

--- CleanNoteTests/EditorInteractorTests.swift
+++ CleanNoteTests/EditorInteractorTests.swift
@@ -17,4 +17,22 @@ class EditorInteractorTests: XCTestCase {
     let expectedText = ""
     XCTAssertEqual(expectedText, output.actualText)
   }
+  
+
+  func test_fetchText_existingNote_verbatimText() {
+    // Arrange.
+    let output = MockEditorInteractorOutput()
+
+    let note = Note(id: "one", text: "sample text")
+    let gateway = SampleNoteGateway(notes: [note])
+
+    let sut = EditorInteractor(output: output, gateway: gateway, noteID: "one")
+
+    // Act.
+    sut.fetchText()
+
+    // Assert.
+    let expectedText = "sample text"
+    XCTAssertEqual(expectedText, output.actualText)
+  }
 }
