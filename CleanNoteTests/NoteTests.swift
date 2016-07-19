3125232c8816282a2319b3c57a1a1174e24a4cfd
Dan Cutting <dan@cutting.io>
2016-07-19 15:52:09 +0100

    NoteTests: equality.

---
+++ CleanNoteTests/NoteTests.swift
@@ -0,0 +1,26 @@
+import XCTest
+@testable import CleanNote
+
+class NoteTests: XCTestCase {
+
+  func test_same_equal() {
+    let note1 = Note(id: "myID", text: "some text")
+    let note2 = Note(id: "myID", text: "some text")
+
+    XCTAssertEqual(note1, note2)
+  }
+
+  func test_differentIDs_unequal() {
+    let note1 = Note(id: "myID", text: "some text")
+    let note2 = Note(id: "yourID", text: "some text")
+
+    XCTAssertNotEqual(note1, note2)
+  }
+
+  func test_differentText_unequal() {
+    let note1 = Note(id: "myID", text: "some text")
+    let note2 = Note(id: "myID", text: "not some text")
+
+    XCTAssertNotEqual(note1, note2)
+  }
+}
