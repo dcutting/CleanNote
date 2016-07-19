ec1fbd151b57ac55b91a1b834cb5266128c73425
Dan Cutting <dan@cutting.io>
2016-07-19 14:35:48 +0100

    Make Note value type Equatable.

--- CleanNote/Code/Note.swift
+++ CleanNote/Code/Note.swift
@@ -4,3 +4,9 @@ struct Note {
   var id: NoteID
   var text: String
 }
+
+extension Note: Equatable {}
+
+func ==(lhs: Note, rhs: Note) -> Bool {
+  return lhs.id == rhs.id && lhs.text == rhs.text
+}
