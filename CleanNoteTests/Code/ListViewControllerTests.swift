538c797e45b88d8e5ae63e837ddca03f153c55f3
Dan Cutting <dan@cutting.io>
2016-07-20 09:27:03 +0100

    ListViewControllerTests: test_update_displaysCorrectNumberOfRowsInTable.

--- CleanNoteTests/Code/ListViewControllerTests.swift
+++ CleanNoteTests/Code/ListViewControllerTests.swift
@@ -4,13 +4,16 @@ import XCTest
 class ListViewControllerTests: XCTestCase {
 
   var interactor: MockListInteractorInput!
+  var tableView: UITableView!
   var sut: ListViewController!
 
   override func setUp() {
     interactor = MockListInteractorInput()
+    tableView = UITableView()
 
     sut = ListViewController()
     sut.interactor = interactor
+    sut.tableView = tableView
   }
 
   func test_viewWillAppear_fetchesNotes() {
@@ -22,5 +25,21 @@ class ListViewControllerTests: XCTestCase {
 
     // Assert.
     XCTAssert(interactor.assert())
+  }
+
+  func test_update_displaysCorrectNumberOfRowsInTable() {
+    // Arrange.
+    let notes = [
+      ListViewNote(id: "1", summary: "sample note"),
+      ListViewNote(id: "2", summary: "another sample note")
+    ]
+
+    // Act.
+    sut.update(notes: notes)
+
+    // Assert.
+    let expectedRows = 2
+    let actualRows = sut.tableView(tableView, numberOfRowsInSection: 0)
+    XCTAssertEqual(expectedRows, actualRows)
   }
 }
