de200e679ca4760ff493cb8bbdaf3957e1df932b
Dan Cutting <dan@cutting.io>
2016-07-21 17:19:29 +0100

    InMemoryNoteGatewayTests: test_fetchNoteWithID_noteFound_returnsNote.

--- CleanNoteTests/Code/InMemoryNoteGatewayTests.swift
+++ CleanNoteTests/Code/InMemoryNoteGatewayTests.swift
@@ -23,15 +23,32 @@ class InMemoryNoteGatewayTests: XCTestCase {
   func test_fetchNoteWithID_noteNotFound_returnsNil() {
     // Arrange.
     let notes = [
-      Note(id: "1", text: "sample note"),
-      Note(id: "2", text: "another sample note")
+      Note(id: "0", text: "sample note"),
+      Note(id: "1", text: "another sample note")
     ]
 
     let sut = InMemoryNoteGateway(notes: notes)
 
     // Act and assert.
-    sut.fetchNote(with: "3") { actualNote in
+    sut.fetchNote(with: "2") { actualNote in
       XCTAssertNil(actualNote)
+    }
+  }
+
+
+  func test_fetchNoteWithID_noteFound_returnsNote() {
+    // Arrange.
+    let notes = [
+      Note(id: "0", text: "sample note"),
+      Note(id: "1", text: "another sample note")
+    ]
+
+    let sut = InMemoryNoteGateway(notes: notes)
+
+    // Act and assert.
+    let expectedNote = notes[1]
+    sut.fetchNote(with: "1") { actualNote in
+      XCTAssertEqual(expectedNote, actualNote)
     }
   }
 }
