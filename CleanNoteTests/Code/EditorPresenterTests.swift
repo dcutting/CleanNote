291fb67153f91b22954d666c917aae43c618ad8c
Dan Cutting <dan@cutting.io>
2016-07-26 18:08:08 +0100

    EditorPresenterTests: test_didFailToSave_showsError.

--- CleanNoteTests/Code/EditorPresenterTests.swift
+++ CleanNoteTests/Code/EditorPresenterTests.swift
@@ -3,17 +3,33 @@ import XCTest
 
 class EditorPresenterTests: XCTestCase {
 
-  func test_didFetchText_updatesInterfaceWithText() {
-    // Arrange.
-    let interface = MockEditorInterface()
-    
-    let sut = EditorPresenter(interface: interface)
+  var interface: MockEditorInterface!
+  var sut: EditorPresenter!
 
+  override func setUp() {
+    interface = MockEditorInterface()
+    sut = EditorPresenter(interface: interface)
+  }
+
+
+  func test_didFetchText_updatesInterfaceWithText() {
     // Act.
     sut.didFetch(text: "my note text")
 
     // Assert.
     let expectedText = "my note text"
     XCTAssertEqual(expectedText, interface.actualUpdateText)
+  }
+
+
+  func test_didFailToSave_showsError() {
+    // Arrange.
+    interface.expectError()
+
+    // Act.
+    sut.didFailToSave()
+
+    // Assert.
+    XCTAssertTrue(interface.assert())
   }
 }
--- CleanNoteTests/Code/MockEditorInterface.swift
+++ CleanNoteTests/Code/MockEditorInterface.swift
@@ -2,13 +2,22 @@
 
 class MockEditorInterface: EditorInterface {
   var actualUpdateText: String?
-  var actualErrorText: String?
+  var shouldExpectError: Bool = false
+  var didCallError: Bool = false
+
+  func expectError() {
+    shouldExpectError = true
+  }
 
   func update(text: String) {
     actualUpdateText = text
   }
 
   func error(text: String) {
-    actualErrorText = text
+    didCallError = true
+  }
+
+  func assert() -> Bool {
+    return shouldExpectError && didCallError
   }
 }
