44eb8d623204b6bbd2296ad5d8a7fe9298f154f4
Dan Cutting <dan@cutting.io>
2016-09-02 09:18:43 +0100

    List tests: test_differentSelection_unequal.

--- CleanNoteCore/CleanNoteCoreTests/Code/ListTests.swift
+++ CleanNoteCore/CleanNoteCoreTests/Code/ListTests.swift
@@ -36,4 +36,22 @@ class ListTests: XCTestCase {
 
     XCTAssertNotEqual(list1, list2)
   }
+
+  func test_differentSelection_unequal() {
+    let notes1 = [
+      Note(id: "myID", text: "some text"),
+      Note(id: "yourID", text: "some other text")
+    ]
+    let selected1 = "myID"
+    let list1 = List(notes: notes1, selected: selected1)
+
+    let notes2 = [
+      Note(id: "myID", text: "some text"),
+      Note(id: "yourID", text: "some other text")
+    ]
+    let selected2 = "yourID"
+    let list2 = List(notes: notes2, selected: selected2)
+
+    XCTAssertNotEqual(list1, list2)
+  }
 }
